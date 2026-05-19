8e121b8ecf884f15661f8c4f21162d1c1844f88a
l06433930-pixel <l06433930@gmail.com>
2026-05-19 23:18:08 +0530

    frontend update

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuthStore } from '../store/useAuthStore';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api', // Should be from env in production
+  baseURL:  import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
   headers: {
     'Content-Type': 'application/json',
   },
